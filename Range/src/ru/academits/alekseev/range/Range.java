64623d01dc4210b51e79b6765bd358d08c89f6d6
platowashere666-ship-it <platowashere666@gmail.com>
2026-01-28 19:27:09 +0300

    Задача Range/Range* попытка №1

--- Range/src/ru/academits/alekseev/range/Range.java
+++ Range/src/ru/academits/alekseev/range/Range.java
@@ -1,4 +1,89 @@
 package ru.academits.alekseev.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double point) {
+        return point >= from && point <= to;
+    }
+
+    public Range getRangeIntersection(Range other) {
+        double maxFrom = Math.max(this.from, other.from);
+        double minTo = Math.min(this.to, other.to);
+
+        if (maxFrom <= minTo) {
+            return new Range(maxFrom, minTo);
+        }
+
+        return null;
+    }
+
+    public Range[] getRangeUnion(Range other) {
+        double left = Math.min(this.from, other.from);
+        double right = Math.max(this.to, other.to);
+
+        if (other.from <= this.to && other.to >= this.from) {
+            Range result = new Range(left, right);
+
+            return new Range[]{result};
+        }
+
+        if (this.from > other.from) {
+            return new Range[]{other, this};
+        }
+
+        return new Range[]{this, other};
+    }
+
+    public Range[] getRangeDifference(Range other) {
+        if (other.to < this.from || other.from > this.to) {
+            return new Range[]{this};
+        }
+
+        if (other.from <= this.from && other.to >= this.to) {
+            return new Range[0];
+        }
+
+        Range[] result = new Range[2];
+        int i = 0;
+
+        if (this.from < other.from) {
+            result[i++] = new Range(this.from, other.from);
+        }
+
+        if (this.to > other.to) {
+            result[i++] = new Range(other.to, this.to);
+        }
+
+        Range[] trimmedResult = new Range[i];
+        System.arraycopy(result, 0, trimmedResult, 0, i);
+
+        return trimmedResult;
+    }
 }
--- Range/src/ru/academits/alekseev/range_main/Main.java
+++ Range/src/ru/academits/alekseev/range_main/Main.java
@@ -1,4 +1,34 @@
 package ru.academits.alekseev.range_main;
 
+import ru.academits.alekseev.range.Range;
+
 public class Main {
+    public static void main(String[] args) {
+        Range a = new Range(2.5, 8.0);
+        Range b = new Range(5.0, 12.0);
+        Range c = new Range(-3.0, 1.0);
+        Range d = new Range(10.0, 15.0);
+
+        System.out.printf("Длина A: %.2f%n", a.getLength());
+        System.out.printf("Длина C: %.2f%n", c.getLength());
+
+        System.out.println("Точка 4.0 входит в A?: " + a.isInside(4.0));
+        System.out.println("Точка 1.5 входит в C?:  " + c.isInside(1.5));
+
+        Range intersection1 = a.getRangeIntersection(b);
+        Range intersection2 = b.getRangeIntersection(d);
+        Range intersection3 = a.getRangeIntersection(d);
+
+        Range[] union1 = a.getRangeUnion(intersection1);
+        Range[] union2 = intersection2.getRangeUnion(intersection1);
+
+        Range[] difference1 = a.getRangeDifference(b);
+        Range[] difference2 = d.getRangeDifference(c);
+
+        a.setFrom(7.2);
+        a.setTo(10.5);
+
+        System.out.println(a.getFrom());
+        System.out.println(a.getTo());
+    }
 }
