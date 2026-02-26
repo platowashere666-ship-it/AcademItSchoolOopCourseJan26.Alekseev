163786ac71e3c5c8fa2b9157e575cb68610ea95c
platowashere666-ship-it <platowashere666@gmail.com>
2026-02-26 21:30:50 +0300

    Задача ArrayListHome, внёс правки по списку

--- ArrayListHome/src/ru/academits/alekseev/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/alekseev/array_list_home/ArrayListHome.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         try (Scanner scanner = new Scanner(new File("ArrayListHome/input.txt"))) {
             ArrayList<String> lines = new ArrayList<>();
 
@@ -15,29 +15,30 @@ public class ArrayListHome {
                 lines.add(scanner.nextLine());
             }
 
-            System.out.println(lines);
+            System.out.println("Список строк из файла: " + lines);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
 
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
 
-        for (int i = 0; i < numbers.size(); ++i) {
+        for (int i = numbers.size() - 1; i >= 0; i--) {
             if (numbers.get(i) % 2 == 0) {
-                //noinspection SuspiciousListRemoveInLoop
                 numbers.remove(i);
             }
         }
 
-        System.out.println(numbers);
+        System.out.println("Список нечётных чисел: " + numbers);
 
-        ArrayList<Integer> numbersToCopy = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
-        ArrayList<Integer> copiedNumbers = new ArrayList<>();
+        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
+        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
 
-        for (Integer numberToCopy : numbersToCopy) {
-            if (!copiedNumbers.contains(numberToCopy)) {
-                copiedNumbers.add(numberToCopy);
+        for (Integer number : numbers2) {
+            if (!uniqueNumbers.contains(number)) {
+                uniqueNumbers.add(number);
             }
         }
 
-        System.out.println(copiedNumbers);
+        System.out.println("Список уникальных чисел: " + uniqueNumbers);
     }
 }
